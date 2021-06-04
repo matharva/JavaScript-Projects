8afd00952d0267409598bdc151b64bd92333641a
Atharva Mohite <atharvamohite20.com>
2021-06-05 02:57:06 +0530

    Hidden SearchBar Done

---
+++ 4-Hidden-SearchBar/app.js
@@ -0,0 +1,6 @@
+let button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+  let searchBtn = document.querySelector(".search");
+  searchBtn.classList.toggle("active");
+});
